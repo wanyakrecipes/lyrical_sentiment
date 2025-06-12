12e4281e0862e242ed55ae5c5c6318d011e8d264
Mark <mwanzalaryan@gmail.com>
2025-06-12 20:32:57 +0100

    fixed bug issue with trbs

--- src/self_labelled_model_comparison.py
+++ src/self_labelled_model_comparison.py
@@ -6,16 +6,11 @@ import pandas as pd
 import openai
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-import re
-import torch
-import roberta_model as roberta
 
 # Read and concaternate labelled data
 song_lyrics_positive_labelled_df = pd.read_csv('../data/processed/song_lyrics_positive_labelled.csv')
 song_lyrics_negative_labelled_df = pd.read_csv('../data/processed/song_lyrics_negative_labelled.csv')
-
 song_lyrics_labelled_df = pd.concat([song_lyrics_positive_labelled_df,song_lyrics_negative_labelled_df])
-
 
 ##### gpt-4o-mini performance #####
 
@@ -51,6 +46,7 @@ def classify_lyric_sentiment_gpt(lyrics):
         return f"Error: {str(e)}"
 
 # Apply the classification function to the DataFrame
+print("Apply sentiment using gpt-4o prompt...")
 song_lyrics_labelled_df["predicted_label_gpt4o"] = song_lyrics_labelled_df["lyrics"].apply(classify_lyric_sentiment_gpt)
 
 #Confusion Matrix
@@ -66,35 +62,4 @@ disp = ConfusionMatrixDisplay(confusion_matrix=cm,
                               display_labels=['negative','positive'])
 
 disp.plot()
-plt.show()
 
-### Load TRBS Model ####
-
-# Automatically use MPS on Mac if available
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-print(f"Using device: {device}")
-
-# Load model and tokenizer once
-print("Load BERT model...")
-tokenizer, model, labels = roberta.load_trbs_model(device = device)
-
-print("Apply senitment over chunks...")
-song_lyrics_labelled_df['prediction_trbs'] = song_lyrics_labelled_df.apply(lambda row: roberta.sentiment_over_chunks(lyrics = row['lyrics'],tokenizer=tokenizer,labels = labels, model = model, device= device), axis=1)
-
-print("Extract binary prediction between positive and negative sentiment...")
-song_lyrics_labelled_df['prediction_trbs_bin_sent_label'] = song_lyrics_labelled_df['prediction_trbs'].apply(lambda x: roberta.collapse_to_binary(x['scores']))
-
-#Confusion Matrix
-accuracy_trbs = accuracy_score(song_lyrics_labelled_df['actual_label'], song_lyrics_labelled_df['prediction_trbs_bin_sent_label'])
-print("TRBS Model results using unseen data")
-print(f"Accuracy: {accuracy_trbs}")
-print("Classification Report:")
-print(classification_report(song_lyrics_labelled_df['actual_label'], song_lyrics_labelled_df['prediction_trbs_bin_sent_label']))
-
-# Print Confusion Matrix
-cm = confusion_matrix(song_lyrics_labelled_df['actual_label'], song_lyrics_labelled_df['prediction_trbs_bin_sent_label'], labels=['negative','positive'])
-disp = ConfusionMatrixDisplay(confusion_matrix=cm,
-                              display_labels=['negative','positive'])
-
-disp.plot()
-plt.show()
---
+++ src/self_labelled_trbs_model.py
@@ -0,0 +1,42 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import trbs_model as trbs
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+# Read and concaternate labelled data
+song_lyrics_positive_labelled_df = pd.read_csv('../data/processed/song_lyrics_positive_labelled.csv')
+song_lyrics_negative_labelled_df = pd.read_csv('../data/processed/song_lyrics_negative_labelled.csv')
+song_lyrics_labelled_df = pd.concat([song_lyrics_positive_labelled_df,song_lyrics_negative_labelled_df])
+
+### Load TRBS Model ####
+
+# Automatically use MPS on Mac if available
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+print(f"Using device: {device}")
+
+# Load model and tokenizer once
+print("Load TRBS model...")
+tokenizer, model, labels = trbs.load_model(device = device)
+
+print("Apply senitment over chunks...")
+song_lyrics_labelled_df['prediction_trbs'] = song_lyrics_labelled_df.apply(lambda row: trbs.sentiment_over_chunks(lyrics = row['lyrics'],tokenizer=tokenizer,labels = labels, model = model, device= device), axis=1)
+
+print("Extract binary prediction between positive and negative sentiment...")
+song_lyrics_labelled_df['prediction_trbs_bin_sent_label'] = song_lyrics_labelled_df['prediction_trbs'].apply(lambda x: trbs.collapse_to_binary(x['scores']))
+
+#Confusion Matrix
+accuracy_trbs = accuracy_score(song_lyrics_labelled_df['actual_label'], song_lyrics_labelled_df['prediction_trbs_bin_sent_label'])
+print("TRBS Model results using unseen data")
+print(f"Accuracy: {accuracy_trbs}")
+print("Classification Report:")
+print(classification_report(song_lyrics_labelled_df['actual_label'], song_lyrics_labelled_df['prediction_trbs_bin_sent_label']))
+
+# Print Confusion Matrix
+cm = confusion_matrix(song_lyrics_labelled_df['actual_label'], song_lyrics_labelled_df['prediction_trbs_bin_sent_label'], labels=['negative','positive'])
+disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                              display_labels=['negative','positive'])
+
+disp.plot()
