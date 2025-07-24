3a14047a76a3c6536246c6f3faf57e92ab85fd2e
Mark <mwanzalaryan@gmail.com>
2025-07-24 21:44:34 +0100

    updated analysis using clean lyrics

--- src/sentiment_per_year_gpt_4o.py
+++ src/sentiment_per_year_gpt_4o.py
@@ -37,9 +37,6 @@ print("Generate sample...")
 #Generate sample of lyrics - only
 song_lyrics_clean_sample_df = song_lyrics_clean_df.copy()
 
-#Songs from 1950 onwards
-song_lyrics_clean_sample_df = song_lyrics_clean_sample_df[(song_lyrics_clean_sample_df['year'] >= 1950)]
-
 #Generate sample based on year
 #TODO need to put a limit on number of songs that can be represented by a single artist, in the set and in a given year.
 song_lyrics_clean_sample_df= song_lyrics_clean_sample_df.groupby('year').apply(lambda x: x.sample(n=100, random_state=42) if len(x) > 100 else x)
@@ -47,13 +44,13 @@ song_lyrics_clean_sample_df = song_lyrics_clean_sample_df.reset_index(drop=True)
 
 print("Number of samples: " + str(len(song_lyrics_clean_sample_df)))
 
-print("Apply sentiment with confidence using gpt-4o...")
-song_lyrics_clean_sample_df['gpt_4o_score'] = song_lyrics_clean_sample_df['lyrics'].apply(gpt_4o.get_lyrics_sentiment_score)
+print("Apply sentiment with confidence using gpt...")
+song_lyrics_clean_sample_df['sentiment_score_gpt'] = song_lyrics_clean_sample_df['clean_lyrics'].apply(gpt_4o.get_lyrics_sentiment_score)
 
 # Make sure sentiment is numeric
-song_lyrics_clean_sample_df['gpt_4o_score'] = pd.to_numeric(song_lyrics_clean_sample_df['gpt_4o_score'], errors='coerce')
+song_lyrics_clean_sample_df['sentiment_score_gpt'] = pd.to_numeric(song_lyrics_clean_sample_df['sentiment_score_gpt'], errors='coerce')
 
 # Group by year
-yearly_sentiment = song_lyrics_clean_sample_df.groupby('year')['gpt_4o_score'].mean().plot(title="Average sentiment decreases over time [gpt-40-mini]",
-                                                                                           ylabel="Sentiment")
+yearly_sentiment = song_lyrics_clean_sample_df.groupby('year')['sentiment_score_gpt'].mean().plot(title="Average sentiment decreases over time (n=6832,gpt-40-mini)",
+                                                                                                  ylabel="Sentiment")
 
